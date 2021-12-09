00ddb2f95eb37292b6c8e0c955707e160a3c2715
Tatsuya Naito <Tatsuya.Naito1@ibm.com>
2021-12-09 11:16:43 +0900

    update i/f

---
+++ api/curl.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -eu
+
+curl 'http://localhost:8000/discovery/search' \
+  -H 'Connection: keep-alive' \
+  -H 'sec-ch-ua: " Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"' \
+  -H 'Accept: application/json, text/plain, */*' \
+  -H 'Content-Type: application/json' \
+  -H 'X-Requested-With: XMLHttpRequest' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36' \
+  -H 'sec-ch-ua-platform: "macOS"' \
+  -H 'Origin: http://localhost:3000' \
+  -H 'Sec-Fetch-Site: same-site' \
+  -H 'Sec-Fetch-Mode: cors' \
+  -H 'Sec-Fetch-Dest: empty' \
+  -H 'Referer: http://localhost:3000/' \
+  -H 'Accept-Language: ja,en-US;q=0.9,en;q=0.8' \
+  --data-raw '{"searchText":"コロナ","category":1}' \
+  --compressed
